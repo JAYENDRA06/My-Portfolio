90e319764f0cc41879ae7737d216985dacafe6f4
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-01-17 00:36:58 +0530

    add: loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-// import Navbar from "./components/Navbar";
-// import Socials from "./components/Socials";
-// import Form from "./components/Form";
-// import AllSkills from "./components/AllSkills";
+import React, { useState, useEffect } from "react";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
 import ProgressBar from "./components/ProgressBar";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <div className="App">
-      <ProgressBar />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <ProgressBar />
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section5 />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loader() {
+    return(
+        <div className="loader">
+            <img src="../../images/temple.svg" alt="loader" />
+            <p>getting your dojo ready</p>
+        </div>
+    )
+}
+
+export default Loader;
