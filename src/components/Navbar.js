1313f179141e9108ea5f8a009e6116c9a4fb1823
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-01-31 01:47:36 +0530

    navbar animation added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Navbar() {
   const navContent = [
@@ -24,8 +24,41 @@ function Navbar() {
     }
   ];
 
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <nav>
+    <nav style={{ top: visible ? '0' : '-100px' }}>
       <ul>
         {navContent.map((content) => (
           <li key={content.navName}>
--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Background1 from "./Background1";
-import Navbar from "./Navbar";
 import Socials from "./Socials";
 
 const description =
   "I am a third year CSE undergrad at vit vellore.i completed my +2 from montfort inter college. my life revolves around learning, coding and coffee but sometimes i need a therapy session of playing football or watching anime , watching Chistopher Nolan’s moives.";
 
 function Section1() {
+
+  const mousePosition = useMousePosition();
+
   return (
     <section className="section1" id="home">
       <Socials />
@@ -21,5 +23,27 @@ function Section1() {
     </section>
   );
 }
+
+const useMousePosition = () => {
+  const [
+    mousePosition,
+    setMousePosition
+  ] = React.useState({ x: null, y: null });
+
+  React.useEffect(() => {
+    const updateMousePosition = ev => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
 
 export default Section1;
